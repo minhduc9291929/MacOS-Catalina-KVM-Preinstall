81cdc5a25a458a6d4b823f39f180bbba2731d8af
ubuntu2310fake <152691685+ubuntu2310fake@users.noreply.github.com>
2023-12-20 21:09:11 +0700

    Update MacOS-Mojave.sh

--- MacOS-Mojave.sh
+++ MacOS-Mojave.sh
@@ -1,24 +1,31 @@
-echo "Checking Virtualization...."
+echo "đang kiểm tra xem máy có bật vt chưa...."
 vtx=$(egrep -i '^flags.*(vmx|svm)' /proc/cpuinfo | wc -l)
-if [ $vtx = 0 ] ; then echo "[Error] Your PC Not Support VT-x / AMD-v. Press Ctrl+C to quit"&& sleep 2711
+if [ $vtx = 0 ] ; then echo "[Lỗi] Máy tính của bạn chưa bật VT-x / AMD-v. bấm Ctrl+C để thoát và vào bios bật vt như trong hướng dẫn trên google"&& sleep 2711
 fi
-echo Installing Software...
+echo "đang cài đặt phần mềm"
 sudo apt update -y
 sudo apt install wget git qemu-system qemu-utils python3 python3-pip -y 
 git clone https://github.com/foxlet/macOS-Simple-KVM.git
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 5900 --region ap &>/dev/null &
+sudo chmod 666 /dev/kvm
 cd macOS-Simple-KVM
 sudo python3  -m pip install click
 ./jumpstart.sh --catalina
-echo "Downloading Mac OS Catalina Disk..." 
-wget -q --show-progress --no-check-certificate -O MyDisk.qcow2 https://tinyurl.com/3s87mjea
+echo "Đang tải về Mac OS Catalina Disk..." 
+wget -q --show-progress --no-check-certificate -O /tmp/MyDisk.qcow2 https://tinyurl.com/3s87mjea
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
 availableRAMcommand="free -m | tail -2 | head -1 | awk '{print \$7}'"
 availableRAM=$(echo $availableRAMcommand | bash)
-echo HEADLESS=1 MEM=$availableRAM"M" CPUS=$cpus SYSTEM_DISK=MyDisk.qcow2 ./headless.sh > silence.sh
+echo HEADLESS=1 MEM=10"G" CPUS=3 SYSTEM_DISK=/tmp/MyDisk.qcow2 ./headless.sh > silence.sh
 chmod +x silence.sh
 nohup ./silence.sh &>/dev/null &
-echo "All Done !!! Your Mac OS VM now Ready to use !!!"
-echo "Open VNC Viewer and connect with port 5900"
-echo "Your VM Specs: $availableRAM"M" / $cpus CPUs"
-echo "User: Catalina-User"
-echo "Pass: 123456"
+echo "tất cả sẵn sàng !!! Máy ảo macOS của bạn sẵn sàng hoạt động !!!"
+echo "Mở VNC Viewer và kết nối với ip theo hướng dẫn"
+echo "Cấu hình của bạn: 10GB Ram / 3core CPU
+echo "tên: Catalina-User"
+echo "mật khẩu: 123456"
+echo "ae vào trang này https://dashboard.ngrok.com/cloud-edge/endpoints để lấy ip vnc"
